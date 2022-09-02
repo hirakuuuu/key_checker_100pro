0dfe42dac4579131d731156f9f3239e28b893960
hirakuuuu <m.hiraku1129@gmail.com>
2022-09-02 17:05:16 +0900

    [fix] ++再度設定を戻してみる

--- djangobot/djangobot/settings.py
+++ djangobot/djangobot/settings.py
@@ -26,9 +26,11 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-
+DATABASES = {
+    'default': dj_database_url.config()
+}
 DEBUG = False
-django_heroku.settings(locals(), secret_key=False)
+
 ALLOWED_HOSTS = []
 
 
