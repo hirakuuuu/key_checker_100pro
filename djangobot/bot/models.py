c3ac6181de5c07a577f5141780a344727d7b21fe
hirakuuuu <m.hiraku1129@gmail.com>
2022-09-03 17:51:56 +0900

    [fix] モデルの仕様変更

--- djangobot/bot/models.py
+++ djangobot/bot/models.py
@@ -5,9 +5,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 class LineUser(models.Model):
     user_id = models.CharField(max_length=34, primary_key=True)
-    x_open = models.SmallIntegerField(default=0, validators=[MinValueValidator(0)])
-    y_open = models.SmallIntegerField(default=0, validators=[MinValueValidator(0)])
-    z_open = models.SmallIntegerField(default=0, validators=[MinValueValidator(0)])
-    x_close = models.SmallIntegerField(default=0, validators=[MinValueValidator(0)])
-    y_close = models.SmallIntegerField(default=0, validators=[MinValueValidator(0)])
-    z_close = models.SmallIntegerField(default=0, validators=[MinValueValidator(0)])
+    x_open = models.SmallIntegerField(default=0)
+    y_open = models.SmallIntegerField(default=0)
+    z_open = models.SmallIntegerField(default=0)
+    x_close = models.SmallIntegerField(default=0)
+    y_close = models.SmallIntegerField(default=0)
+    z_close = models.SmallIntegerField(default=0)
