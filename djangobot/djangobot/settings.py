75d2889696aff85cfbcef77addfb57a3ffe560cd
hirakuuuu <m.hiraku1129@gmail.com>
2022-09-03 14:14:22 +0900

    [fix] ローカルでアドミンが確認できるように修正

--- djangobot/djangobot/settings.py
+++ djangobot/djangobot/settings.py
@@ -22,13 +22,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+# SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
@@ -44,6 +44,11 @@ DATABASES = {
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+try:
+ from .local_settings import *
+except ImportError:
+ pass
 
 # Application definition
 
