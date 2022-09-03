62e382da8805d9382b5d9954fb463fd1b191a805
hirakuuuu <m.hiraku1129@gmail.com>
2022-09-03 14:27:56 +0900

    [fix] モデルの使用を変更

--- djangobot/bot/models.py
+++ djangobot/bot/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone
+from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
@@ -11,4 +11,3 @@ class User(models.Model):
     x_close = models.SmallIntegerField(default=0, validators=[MinValueValidator(0)])
     y_close = models.SmallIntegerField(default=0, validators=[MinValueValidator(0)])
     z_close = models.SmallIntegerField(default=0, validators=[MinValueValidator(0)])
-    created_date = models.DateTimeField(default=timezone.now())
